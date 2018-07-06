a50972ba64fe7fb006503dc2317e469d76eca78b
josdoaitran <josdoaitran@gmail.com>
2018-07-06 22:52:03 +0700

    update pom

--- src/main/java/base/Initialize.java
+++ src/main/java/base/Initialize.java
@@ -1,4 +1,9 @@
 package base;
 
 public class Initialize {
+    public static Logger logger = Logger.getLogger(BaseInit.class.getSimpleName());
+    public static WebDriver driver = null;
+    protected static AppiumDriver mobileDriver = null;
+
+    protected static List<WebDriver> driverList = new ArrayList<WebDriver>();
 }
