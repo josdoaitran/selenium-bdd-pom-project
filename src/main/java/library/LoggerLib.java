514004ee415028148ef7d8d4f276bb522ea878df
josdoaitran <josdoaitran@gmail.com>
2018-07-07 15:17:31 +0700

    update logger

--- src/main/java/library/LoggerLib.java
+++ src/main/java/library/LoggerLib.java
@@ -1,6 +1,6 @@
 package library;
 
-import java.util.logging.Logger;
+import org.apache.log4j.Logger;
 
 public class LoggerLib {
     private static Logger logger = Logger.getLogger("LOG");
@@ -10,26 +10,30 @@ public class LoggerLib {
     }
 
     public static void printLogActionInPage(String pageDescription){
-        logger.info("  ----- " + pageDescription + " -----");
+        logger.info("----- " + pageDescription + " -----");
+    }
+
+    public static void printErrorLogActionInPage(String pageDescription){
+        logger.error("----- " + pageDescription + " -----");
     }
 
     public static void printLogActionOfElement(String elementAction, String elementValue ){
-        logger.info("    +++++ " + elementAction + ": " + elementValue + " +++++");
+        logger.info("+++++ " + elementAction + ": " + elementValue + " +++++");
     }
 
     public static void printLogParameterInput(String parameterDescription, String parameterValue){
-        logger.info("      ***** " + parameterDescription + ":" + parameterValue + " *****");
+        logger.info("***** " + parameterDescription + ":" + parameterValue + " *****");
     }
 
     public static void printLogParameterGetProperties(String parameterDescription, String parameterValue){
-        logger.info("      ##### " + parameterDescription + ":" + parameterValue + " #####");
+        logger.info("##### " + parameterDescription + ":" + parameterValue + " #####");
     }
 
     public static void printLogParameterGetExcelFile(String parameterDescription, String parameterValue){
-        logger.info("      $$$$$ " + parameterDescription + ":" + parameterValue + " $$$$$");
+        logger.info("$$$$$ " + parameterDescription + ":" + parameterValue + " $$$$$");
     }
 
     public static void printLogParameterGetTestNGFile(String parameterDescription, String parameterValue){
-        logger.info("      ^^^^^ " + parameterDescription + ":" + parameterValue + " ^^^^^");
+        logger.info("^^^^^ " + parameterDescription + ":" + parameterValue + " ^^^^^");
     }
 }
