236ce28208ff3c3fa4037daa5182ac1e41cf15f4
josdoaitran <josdoaitran@gmail.com>
2018-07-07 15:11:07 +0700

    update lib

--- src/main/java/library/LoggerLib.java
+++ src/main/java/library/LoggerLib.java
@@ -1,4 +1,35 @@
 package library;
 
+import java.util.logging.Logger;
+
 public class LoggerLib {
+    private static Logger logger = Logger.getLogger("LOG");
+
+    public static void printLogStepDefinitions(String stepDefinitions){
+        logger.info("===== " + stepDefinitions + " =====");
+    }
+
+    public static void printLogActionInPage(String pageDescription){
+        logger.info("  ----- " + pageDescription + " -----");
+    }
+
+    public static void printLogActionOfElement(String elementAction, String elementValue ){
+        logger.info("    +++++ " + elementAction + ": " + elementValue + " +++++");
+    }
+
+    public static void printLogParameterInput(String parameterDescription, String parameterValue){
+        logger.info("      ***** " + parameterDescription + ":" + parameterValue + " *****");
+    }
+
+    public static void printLogParameterGetProperties(String parameterDescription, String parameterValue){
+        logger.info("      ##### " + parameterDescription + ":" + parameterValue + " #####");
+    }
+
+    public static void printLogParameterGetExcelFile(String parameterDescription, String parameterValue){
+        logger.info("      $$$$$ " + parameterDescription + ":" + parameterValue + " $$$$$");
+    }
+
+    public static void printLogParameterGetTestNGFile(String parameterDescription, String parameterValue){
+        logger.info("      ^^^^^ " + parameterDescription + ":" + parameterValue + " ^^^^^");
+    }
 }
