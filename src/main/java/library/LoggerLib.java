a6f85098e8d74450d4c50a1bbb8ec310e8add4d5
josdoaitran <josdoaitran@gmail.com>
2018-07-07 15:20:28 +0700

    update

--- src/main/java/library/LoggerLib.java
+++ src/main/java/library/LoggerLib.java
@@ -5,6 +5,10 @@ import org.apache.log4j.Logger;
 public class LoggerLib {
     private static Logger logger = Logger.getLogger("LOG");
 
+    public static void printLogFrameworkSteps(String frameworkStep){
+        logger.info("===== " + frameworkStep + " =====");
+    }
+
     public static void printLogStepDefinitions(String stepDefinitions){
         logger.info("===== " + stepDefinitions + " =====");
     }
@@ -15,10 +19,6 @@ public class LoggerLib {
 
     public static void printErrorLogActionInPage(String pageDescription){
         logger.error("----- " + pageDescription + " -----");
-    }
-
-    public static void printLogActionOfElement(String elementAction, String elementValue ){
-        logger.info("+++++ " + elementAction + ": " + elementValue + " +++++");
     }
 
     public static void printLogParameterInput(String parameterDescription, String parameterValue){
@@ -33,7 +33,4 @@ public class LoggerLib {
         logger.info("$$$$$ " + parameterDescription + ":" + parameterValue + " $$$$$");
     }
 
-    public static void printLogParameterGetTestNGFile(String parameterDescription, String parameterValue){
-        logger.info("^^^^^ " + parameterDescription + ":" + parameterValue + " ^^^^^");
-    }
 }
