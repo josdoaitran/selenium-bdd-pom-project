7cfa9f524994a0993da1c8f72fde8389176a91c3
josdoaitran <josdoaitran@gmail.com>
2018-07-09 16:08:38 +0700

    update

--- src/main/java/library/GeneralUtilities.java
+++ src/main/java/library/GeneralUtilities.java
@@ -1,11 +1,17 @@
 package library;
 
+import org.apache.log4j.Logger;
+
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.io.*;
+import java.util.Properties;
 
 import static library.LoggerLib.printLogFrameworkSteps;
 
 public class GeneralUtilities {
+    private static Logger logger = Logger.getLogger(GeneralUtilities.class.getSimpleName());
+
     public static void focusWebBrowser() {
         if (Generic.runBrowser.equalsIgnoreCase("chrome")) {
             pressHotKeyChrome();
@@ -50,6 +56,56 @@ public class GeneralUtilities {
         } catch (AWTException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * Description: Method to read the configuration from Config file.
+     *
+     * @param sFile FILE Destination
+     * @param sKey  KeyWord to read value.
+     * @return
+     */
+    public static String getConfigValue(String sFile, String sKey) {
+        logger.info("**** Read Configuration file ****");
+        Properties prop = new Properties();
+        String sValue = null;
+        try {
+            InputStream input = new FileInputStream(sFile);
+            prop.load(input);
+            sValue = prop.getProperty(sKey);
+            logger.info("***** Value from Properties file of Parameter: " + sKey + ": " + sValue);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            logger.info("***** Can not find the properties file ****" + sValue);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sValue;
+    }
+
+    /**
+     * Description: Method to set the configuration from Config file.
+     *
+     * @param sFile
+     * @param sKey
+     * @param sValue
+     */
+    public static void setConfigValue(String sFile, String sKey, String sValue) {
+        logger.info("***** Read Configuration file *****");
+        Properties prop = new Properties();
+        try {
+            FileInputStream fis = new FileInputStream(new File(sFile));
+            prop.load(fis);
+            fis.close();
+            FileOutputStream fos = new FileOutputStream(new File(sFile));
+            prop.setProperty(sKey, sValue);
+            prop.store(fos, "Updating folder path");
+            fos.close();
+            logger.info("**** Value from Properties file of Parameter: " + sKey + "be set: " + sValue);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/library/Generic.java
+++ src/main/java/library/Generic.java
@@ -3,8 +3,10 @@ package library;
 import base.Initialize;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -12,6 +14,7 @@ import org.openqa.selenium.safari.SafariDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 import static library.LoggerLib.printLogFrameworkSteps;
@@ -24,6 +27,11 @@ public class Generic extends Initialize {
     public static String sBrowser = "Chrome";
     public static String sOS = "Windows";
     public static String sVersion = "60";
+
+    public static String sDirPath = System.getProperty("user.dir");
+    public static String SELENIUM_GRID_HUB = "http://192.168.1.213:4444/wd/hub";
+    private static String OS = System.getProperty("os.name");
+    public final static String PROPERTIES_FILE = sDirPath + "/Primary.properties";
 
     private static void startChromeDriver(){
         System.setProperty("webdriver.chrome.driver", sDirPath + "/src/test/resources/webDrivers/chromedriver.exe");
@@ -66,6 +74,7 @@ public class Generic extends Initialize {
             logger.info("***** Browser: " + Generic.sBrowser);
             logger.info("***** OS: " + Generic.sOS);
             logger.info("***** Version: " + Generic.sVersion);
+            setSeleniumGridHub();
             if (Generic.sBrowser.equalsIgnoreCase("chrome")) {
                 ChromeOptions options = Generic.setOptionsForChrome();
                 driver = new RemoteWebDriver(new URL(SELENIUM_GRID_HUB), options);
@@ -99,5 +108,59 @@ public class Generic extends Initialize {
             runBrowser = "chrome";
         }
         logger.info("***** We will start test with browser: " + runBrowser);
+    }
+
+    /*
+   Update for method: setDownload Location on Chrome
+    */
+    private static ChromeOptions setOptionsForChrome() {
+        String downloadFilepath = Generic.sDirPath + "/src/test/resources/testData/download/";
+        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        chromePrefs.put("download.default_directory", downloadFilepath);
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", chromePrefs);
+        options.addArguments("disable-infobars");
+        return options;
+    }
+    /*
+    Update for method: setDownload Location on Chrome
+     */
+    private static FirefoxOptions setOptionsForFirefox() {
+        FirefoxProfile profile = new FirefoxProfile();
+        profile.setPreference("browser.download.manager.showWhenStarting", false);
+        String downloadFilepath = Generic.sDirPath + "/src/test/resources/testData/download/";
+        profile.setPreference("browser.helperApps.alwaysAsk.force", false);
+        profile.setPreference("browser.download.dir", downloadFilepath);
+        profile.setPreference("browser.download.folderList", 2);
+        profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "application/zip");
+        profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "application/x-gzip, application/csv, text/csv," +
+                "image/jpeg, image/png, application/zip, text/plain, application/msword, image/jpg," +
+                "application/vnd.openxmlformats-officedocument.wordprocessingml.document, " +
+                "application/vnd.openxmlformats-officedocument.wordprocessingml.document, " +
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        profile.setPreference("browser.download.manager.showWhenStarting", false);
+        profile.setPreference("pdfjs.disabled", true);
+        profile.setPreference("network.proxy.type", 0);
+        profile.setAcceptUntrustedCertificates(true);
+        profile.setAssumeUntrustedCertificateIssuer(true);
+        //Disable OCSP for accessing Zimbra Email
+        profile.setPreference("security.OCSP.enabled", 0);
+        FirefoxOptions options = new FirefoxOptions();
+        options.setProfile(profile);
+        options.addArguments("test-type");
+        return options;
+    }
+    private static void setSeleniumGridHub(){
+        SELENIUM_GRID_HUB = GeneralUtilities.getConfigValue(convertDirectory(Generic.PROPERTIES_FILE), "LOCAL_JENKINS");
+    }
+    public static String  convertDirectory(String directory){
+        if (OS.contains("Windows")){
+            logger.info("***** We are working on Windows environment: " + OS);
+            directory = directory.replace("/","\\");
+        }else {
+            logger.info("***** We are working on Linux / Mac environment: " + OS);
+        }
+        return directory;
     }
 }
